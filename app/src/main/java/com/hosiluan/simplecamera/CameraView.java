a278d982e8a6925438830f43602d26712fdc60a9
unknown <luanho192@gmail.com>
2017-11-08 17:15:52 +0700

    commit at company on 8/11/2017

--- app/src/main/java/com/hosiluan/simplecamera/CameraView.java
+++ app/src/main/java/com/hosiluan/simplecamera/CameraView.java
@@ -2,13 +2,27 @@ package com.hosiluan.simplecamera;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.Policy;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 /**
  * Created by Ho Si Luan on 11/7/2017.
@@ -20,8 +34,13 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
     private Camera mCamera;
 
     public boolean isFlashOn;
+    public Bitmap mLastestThumnailBitmap;
     private boolean hasFlash;
     Camera.Parameters params;
+    int currentZoomLevel = 1;
+
+    private TakePhotoListener mTakePhotoListener;
+
 
     public boolean isFlashOn() {
         return isFlashOn;
@@ -128,6 +147,28 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+    public void takePhoto() {
+
+        params = mCamera.getParameters();
+
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size size = sizes.get(0);
+        for (int i = 0; i < sizes.size(); i++) {
+            if (sizes.get(i).width > size.width)
+                size = sizes.get(i);
+        }
+
+        params.setPictureSize(size.width, size.height);
+
+        mCamera.setParameters(params);
+        if (isFlashOn) {
+            turnOnFlash();
+        }
+        mCamera.takePicture(myShutterCallback, myPictureCallback_RAW, mPicture);
+        turnOffFlash();
+
+    }
+
 
     // Get the camera
     private void getCamera() {
@@ -143,33 +184,170 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
     // Turning On flash
     private void turnOnFlash() {
-        if (!isFlashOn) {
-            if (mCamera == null) {
-                return;
-            }
-
-            params = mCamera.getParameters();
-            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            mCamera.setParameters(params);
-            isFlashOn = true;
-
+        if (mCamera == null){
+            return;
         }
 
+        params = mCamera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        mCamera.setParameters(params);
     }
 
     // Turning Off flash
     private void turnOffFlash() {
-        if (isFlashOn) {
-            if (mCamera == null) {
+
+        if (mCamera == null) {
+            return;
+        }
+        params = mCamera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        mCamera.setParameters(params);
+    }
+
+
+    Camera.ShutterCallback myShutterCallback = new Camera.ShutterCallback() {
+
+        @Override
+        public void onShutter() {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    Camera.PictureCallback myPictureCallback_RAW = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] arg0, Camera arg1) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                Log.d("Luan", "Error creating media file, check storage permissions: ");
                 return;
             }
+            String path = pictureFile.getAbsolutePath();
 
-            params = mCamera.getParameters();
-            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-            mCamera.setParameters(params);
-            isFlashOn = false;
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+
+
+                File file = new File(path);
+                Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(pictureFile));
+
+                mTakePhotoListener.setBitmap(bitmap);
+
+
+            } catch (FileNotFoundException e) {
+                Log.d("Luan", "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d("Luan", "Error accessing file: " + e.getMessage());
+            }
+
+            refreshCamera();
+
+        }
+    };
+
+    public void refreshCamera() {
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+
         }
     }
 
 
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+
+    public void zoomIn(){
+        params= mCamera.getParameters();
+        if (params.isZoomSupported()){
+            int maxZoomLevel = params.getMaxZoom();
+            Log.d("Luan","max zoom level " + maxZoomLevel);
+            if (currentZoomLevel < maxZoomLevel-5){
+                currentZoomLevel+= 5;
+                params.setZoom(currentZoomLevel);
+//                params.setWhiteBalance(Camera.Parameters.WHITE_BALANCE_CLOUDY_DAYLIGHT);
+                params.setExposureCompensation(params.getMaxExposureCompensation());
+                mCamera.setParameters(params);
+            }
+        }
+    }
+
+    public void zoomOut(){
+        params= mCamera.getParameters();
+        if (params.isZoomSupported()) {
+            if (currentZoomLevel >= 5){
+                currentZoomLevel -= 5;
+                params.setZoom(currentZoomLevel);
+                mCamera.setParameters(params);
+            }
+        }
+    }
+
+
+    public interface TakePhotoListener{
+        void setBitmap(Bitmap bitmap);
+    }
+
+    public void setListener(TakePhotoListener mTakePhotoListener){
+        this.mTakePhotoListener = mTakePhotoListener;
+    }
 }
--- app/src/main/java/com/hosiluan/simplecamera/MainActivity.java
+++ app/src/main/java/com/hosiluan/simplecamera/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hosiluan.simplecamera;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,18 +12,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements CameraView.TakePhotoListener {
 
     private Camera mCamera = null;
     private CameraView mCameraView = null;
 
+    public ImageView mLastestThumnailImageView;
     private Button mSwitchCameraButton;
-    private ImageButton mSwitchCameraImageButton, mChangeFlashImageButton;
+    private ImageButton mSwitchCameraImageButton, mChangeFlashImageButton, mTakePhotoImageButton,
+            mZoomInImageButton, mZoomOutImageButton;
     private int mCurrentCameraId = 0;
-
 
 
     @Override
@@ -31,17 +39,59 @@ public class MainActivity extends BaseActivity {
         checkReadtoragePermision();
         checkWriteStoragePermision();
 
-
         setView();
         setEvent();
+    }
 
-        // get the camera
-//        getCamera();
-
-        // displaying button image
-//        toggleButtonImage();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCameraView.setListener(this);
+//        mLastestThumnailImageView.setImageResource(R.drawable.ic_flash_on);
+//
+//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+//
+//
+//        File file = new File(mediaStorageDir.getAbsolutePath() + File.separator +"IMG_20171108_144554.jpg");
+//        Log.d("Luan",file.getAbsolutePath()  + " path");
+//
+//        FileInputStream streamIn = null;
+//        try {
+//            streamIn = new FileInputStream(file);
+//        } catch (FileNotFoundException e) {
+//            Log.d("Luan","hello there");
+//            Log.d("Luan",e.toString());
+//            e.printStackTrace();
+//        }
+//
+//        Bitmap bitmap = BitmapFactory.decodeStream(streamIn);
+//        if (bitmap == null){
+//            Log.d("Luan","bitmap null");
+//        }else {
+//            Log.d("Luan","khac null");
+//        }
+//        bitmap = getScaledBitmap(bitmap,bitmap.getWidth(),bitmap.getHeight());
+//
+//        mLastestThumnailImageView.setImageBitmap(bitmap);
 
     }
+
+    public static Bitmap getScaledBitmap(Bitmap b, int reqWidth, int reqHeight) {
+        int bWidth = b.getWidth();
+        int bHeight = b.getHeight();
+
+        int nWidth = reqWidth;
+        int nHeight = reqHeight;
+
+        float parentRatio = (float) reqHeight / reqWidth;
+
+        nHeight = bHeight;
+        nWidth = (int) (reqWidth * parentRatio);
+
+        return Bitmap.createScaledBitmap(b, nWidth, nHeight, true);
+    }
+
 
     @Override
     protected void onPause() {
@@ -49,6 +99,7 @@ public class MainActivity extends BaseActivity {
         // on pause turn off the flash
 //        turnOffFlash();
     }
+
 
     @Override
     protected void onStop() {
@@ -61,9 +112,14 @@ public class MainActivity extends BaseActivity {
     }
 
     private void setView() {
-//        mSwitchCameraButton = findViewById(R.id.btn_switch_camera);
         mSwitchCameraImageButton = findViewById(R.id.img_btn_switch_camera);
         mChangeFlashImageButton = findViewById(R.id.img_btn_switch_flash);
+        mTakePhotoImageButton = findViewById(R.id.img_btn_take_photo);
+        mLastestThumnailImageView = findViewById(R.id.img_lastest_thumnail);
+        mZoomOutImageButton = findViewById(R.id.img_btn_zoom_out);
+        mZoomInImageButton = findViewById(R.id.img_btn_zoom_in);
+
+
         mCamera = Camera.open();
 
         if (mCamera != null) {
@@ -93,9 +149,28 @@ public class MainActivity extends BaseActivity {
         mChangeFlashImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                mCameraView.changeFlashStatus();
                 toggleButtonImage();
+            }
+        });
+
+        mTakePhotoImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCameraView.takePhoto();
+            }
+        });
+
+        mZoomInImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCameraView.zoomIn();
+            }
+        });
+
+        mZoomOutImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCameraView.zoomOut();
             }
         });
     }
@@ -107,11 +182,20 @@ public class MainActivity extends BaseActivity {
     private void toggleButtonImage() {
         if (mCameraView.isFlashOn()) {
             mChangeFlashImageButton.setImageResource(R.drawable.ic_flash_off);
+            mCameraView.setFlashOn(false);
         } else {
             Log.d("Luan", "toggle");
-
+            mCameraView.setFlashOn(true);
             mChangeFlashImageButton.setImageResource(R.drawable.ic_flash_on);
         }
     }
 
+    @Override
+    public void setBitmap(Bitmap bitmap) {
+        if (bitmap != null) {
+            bitmap = getScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight());
+            mLastestThumnailImageView.setImageBitmap(bitmap);
+            mLastestThumnailImageView.setRotation(90);
+        }
+    }
 }
